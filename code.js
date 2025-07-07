f25279523beb6188e51ab4464bdcb61c7a654c03
li-kravt <215831814+li-kravt@users.noreply.github.com>
2025-07-07 16:28:51 +0200

    feat: add function locationRostov() for convenience

--- code.js
+++ code.js
@@ -1,12 +1,25 @@
-const latitude = 52.52
-const longitude = 13.41
+// const latitude = 52.52
+// const longitude = 13.41
 
-const result = fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`)
+// const result = fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`)
 
 
-result
-.then(res => res.json())
-.then(res => console.log(res)
-)
+// result
+// .then(res => res.json())
+// .then(res => console.log(res))
 
-console.log('hi')
+// console.log('hi')
+
+
+function locationRostov() {
+    const latitude = 47.2313
+    const longitude = 39.7233
+
+    let res = ""
+    
+    res = `latitude=${latitude}&longitude=${longitude}`
+    return res
+}
+
+const tempRostov = fetch(`https://api.open-meteo.com/v1/forecast?${locationRostov()}&hourly=temperature_2m&forecast_days=1`)
+
