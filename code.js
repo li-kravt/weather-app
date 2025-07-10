20f289f6305faf566c3350b2859b05873f531435
li-kravt <215831814+li-kravt@users.noreply.github.com>
2025-07-10 17:59:42 +0200

    refactor: add universal displayDataWeather() instead of repeating code

--- code.js
+++ code.js
@@ -15,22 +15,29 @@ function locationRostov() {
     const latitude = 47.2313
     const longitude = 39.7233
     
-    let res = `latitude=${latitude}&longitude=${longitude}`
-    return res
+    return res = `latitude=${latitude}&longitude=${longitude}`
 }
 
 function locationMunich() {
     const latitude = 48.1374
     const longitude = 11.5755
 
-    let res = `latitude=${latitude}&longitude=${longitude}`
-    return res
+    return res = `latitude=${latitude}&longitude=${longitude}`
+}
+
+function locationTelAviv() {
+    const latitude = 32.0809
+    const longitude = 34.7806
+
+    return res = `latitude=${latitude}&longitude=${longitude}`
 }
 
 
 const tempRostov = fetch(`https://api.open-meteo.com/v1/forecast?${locationRostov()}&hourly=temperature_2m&forecast_days=1`)
+const tempMunich = fetch(`https://api.open-meteo.com/v1/forecast?${locationMunich()}&hourly=temperature_2m&forecast_days=1`)
 
 const forecast = document.getElementById("forecast")
+
 
 function codeWeatherCity(city) {
 
@@ -46,54 +53,41 @@ var dataWeather = `<div id="city-data" class="flex flex-col items-center gap-4">
 return forecast.innerHTML += dataWeather
 }
 
+function displayDataWeather(city, data) {
+    
+const temperatureСity = document.getElementById(`temperature${city}`)
+const timeCity = document.getElementById(`time${city}`)
+const dateCity = document.getElementById(`date${city}`)
+
+dateCity.innerHTML += data.hourly.time[0].split("T")[0]
+
+data.hourly.temperature_2m.forEach(element => {
+        
+temperatureСity.innerHTML += `${Math.round(element) + "°C" + "<br>"}`
+});
+
+data.hourly.time.forEach(element => {
+    const dateTime = element.split("T")
+    const simpleHour = dateTime[1].split(":")[0]
+    
+    timeCity.innerHTML += `${simpleHour + "<br>"}`
+});
+
+}
 
 tempRostov.then(response => response.json())
 .then(data => { 
 
     codeWeatherCity("Rostov")
+    displayDataWeather("Rostov", data)
 
-const temperatureRostov = document.getElementById("temperatureRostov")
-const timeRostov = document.getElementById("timeRostov")
-const date = document.getElementById("date")
-
-    
-    const curDate = data.hourly.time[0].split("T")[0]
-    dateRostov.innerHTML += curDate
-    
-    data.hourly.temperature_2m.forEach(element => {
-        
-        temperatureRostov.innerHTML += `${Math.round(element) + "°C" + "<br>"}`
-    });
-    
-    data.hourly.time.forEach(element => {
-        const dateTime = element.split("T")
-        const simpleHour = dateTime[1].split(":")[0]
-        
-        timeRostov.innerHTML += `${simpleHour + "<br>"}`
-    });
-    
 })
 
-
-const tempMunich = fetch(`https://api.open-meteo.com/v1/forecast?${locationMunich()}&hourly=temperature_2m&forecast_days=1`)
+//------------------------------------------------------------------------------------------------------------------------------
 tempMunich.then(response => response.json())
 .then(data => {
     
     codeWeatherCity("Munich")
-
-    const temperatureMunich = document.getElementById("temperatureMunich")
-    const timeMunich = document.getElementById("timeMunich")
-    const date = document.getElementById("date")
-
-    dateMunich.innerHTML += `${data.hourly.time[0].split("T")[0]}`
-
-    data.hourly.temperature_2m.forEach(element =>{
-        temperatureMunich.innerHTML += `${Math.round(element) + "°C" + "<br>"}`
-    })
-
-    data.hourly.time.forEach(element => {
-        const simpleHour = element.split("T")[1].split(":")[0]
-        timeMunich.innerHTML += `${simpleHour + "<br>"}`
-    })
+    displayDataWeather("Munich", data)
 
 })
