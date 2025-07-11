3324ad7dbd8b4bca8b8d7516634fe6ff079f15f3
li-kravt <215831814+li-kravt@users.noreply.github.com>
2025-07-12 00:58:19 +0300

    feat: add new logic - not working yet

--- code.js
+++ code.js
@@ -25,17 +25,98 @@ function locationMunich() {
     return res = `latitude=${latitude}&longitude=${longitude}`
 }
 
-function locationTelAviv() {
+function TelAviv() {
+
     const latitude = 32.0809
     const longitude = 34.7806
 
-    return res = `latitude=${latitude}&longitude=${longitude}`
+    return fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&forecast_days=1`)
 }
 
+//не работает - додумать логику. Идея: имя в самой функции. Что-то не так
+function fetchCity(city, latitude, longitude) {
+
+    const latitude = 32.0809
+    const longitude = 34.7806
+
+    return fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&forecast_days=1`)
+}
+
+const tempOffline = () => new Promise(resolve => setTimeout(() => {
+    resolve({
+        "latitude": 32.125,
+        "longitude": 34.8125,
+        "generationtime_ms": 0.017523765563964844,
+        "utc_offset_seconds": 0,
+        "timezone": "GMT",
+        "timezone_abbreviation": "GMT",
+        "elevation": 11.0,
+        "hourly_units": {
+            "time": "iso8601",
+            "temperature_2m": "°C"
+        },
+        "hourly": {
+            "time": [
+                "2025-07-10T00:00",
+                "2025-07-10T01:00",
+                "2025-07-10T02:00",
+                "2025-07-10T03:00",
+                "2025-07-10T04:00",
+                "2025-07-10T05:00",
+                "2025-07-10T06:00",
+                "2025-07-10T07:00",
+                "2025-07-10T08:00",
+                "2025-07-10T09:00",
+                "2025-07-10T10:00",
+                "2025-07-10T11:00",
+                "2025-07-10T12:00",
+                "2025-07-10T13:00",
+                "2025-07-10T14:00",
+                "2025-07-10T15:00",
+                "2025-07-10T16:00",
+                "2025-07-10T17:00",
+                "2025-07-10T18:00",
+                "2025-07-10T19:00",
+                "2025-07-10T20:00",
+                "2025-07-10T21:00",
+                "2025-07-10T22:00",
+                "2025-07-10T23:00"
+            ],
+            "temperature_2m": [
+                24.1,
+                23.5,
+                23.0,
+                22.8,
+                23.5,
+                25.2,
+                27.5,
+                28.9,
+                29.9,
+                29.5,
+                30.0,
+                30.0,
+                30.1,
+                29.6,
+                29.1,
+                28.5,
+                27.9,
+                27.1,
+                26.8,
+                26.5,
+                26.1,
+                25.9,
+                25.7,
+                25.5
+            ]
+        }
+    })
+}, 3_000))
+
+tempOffline().then(res => console.log(res))
 
 const tempRostov = fetch(`https://api.open-meteo.com/v1/forecast?${locationRostov()}&hourly=temperature_2m&forecast_days=1`)
 const tempMunich = fetch(`https://api.open-meteo.com/v1/forecast?${locationMunich()}&hourly=temperature_2m&forecast_days=1`)
-const tempTelAviv = fetch(`https://api.open-meteo.com/v1/forecast?${locationTelAviv()}&hourly=temperature_2m&forecast_days=1`)
+// const tempTelAviv = fetch(`https://api.open-meteo.com/v1/forecast?${locationTelAviv()}&hourly=temperature_2m&forecast_days=1`)
 
 const forecast = document.getElementById("forecast")
 
@@ -93,10 +174,35 @@ tempMunich.then(response => response.json())
 
 })
 
-tempTelAviv.then(response => response.json())
+locationTelAviv.then(response => response.json())
 .then(data => {
     
     codeWeatherCity("TelAviv")
     displayDataWeather("TelAviv", data)
 
 })
+
+//функция для вывода данных в html. Первый шаг: function fetchCity(latitude, longitude), далее 
+
+function dataDisplayWeatherCity(city){
+    city.then(response => response.json())
+    .then(data => { 
+        codeWeatherCity(`"${city}"`)
+        displayDataWeather(`"${city}"`, data)
+    })
+}
+
+//функция для получения данных в html сразу. Используя название грода, широту и долготу. Проверить. Нужен return? нет наверное
+function dataDisplayWeatherCity(city, latitude, longitude){
+
+        const latitude = 32.0809
+        const longitude = 34.7806
+    
+        fetchCity = fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&forecast_days=1`)
+    
+    fetchCity.then(response => response.json())
+    .then(data => { 
+        codeWeatherCity(`"${city}"`)
+        displayDataWeather(`"${city}"`, data)
+    })
+}
